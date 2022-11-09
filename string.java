e8beced403774a13dc3bd1b1327550e61637bdfc
kunalquantum <87424424+kunalquantum@users.noreply.github.com>
2022-11-09 20:07:14 +0530

    Implementation of String function in java
    A number of methods provided in Java to perform operations in Strings are called String functions. The methods are compare (), concat (), equals (), split (), length (), replace (), compareTo () and so on. Strings in Java are constant, and it is created either using a literal or using a keyword.

---
+++ string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class string
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+String name=sc.nextLine();
+String name1=sc.nextLine();
+System.out.println(name.length());
+System.out.println(name.concat(name1));
+
+System.out.println(name.charAt(4));
+System.out.println(name.indexOf("a"));
+}
+}
+
+
+
